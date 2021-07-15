f96babd6948b06fc34bf4a7b1a41d8906481af1b
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2021-07-15 03:08:20 +0300

    reworked graphview, got finale version of app

--- graph.py
+++ graph.py
@@ -1,10 +1,7 @@
 import numpy as np
-from celluloid import Camera
-from matplotlib import pyplot as plt
 
-TAU = np.linspace(0, 1, 1000)  # 1000 of points from [0; 1000]
 
-def from_polar(p_pts):
+def from_polar(p_pts):  # translate points in polar system to descartes
     r = p_pts[:, 0]
     phi = p_pts[:, 1]
     x = r * np.cos(phi)
@@ -12,10 +9,10 @@ def from_polar(p_pts):
     return np.array([x, y]).T
 
 
-def bezi(P, t):  # core function that evalueat
+def bezi(P, t):  # core function that evaluate curves
     MB = np.matrix([
+        [3, -6, 3, 0],  # https://en.wikipedia.org/wiki/Bézier_curve
         [-1, 3, -3, 1],
-        [3, -6, 3, 0],
         [-3, 3, 0, 0],
         [1, 0, 0, 0]
     ])
@@ -25,32 +22,3 @@ def bezi(P, t):  # core function that evalueat
     T_mat = np.power(yy, xx)  # yy in power of xx
     B = T_mat * MB * P  # get coords
     return B  # returns coords
-
-
-def new_plot(P, polar=False):  # P - nparray of points that must init like np.array([[1,0], [2, 3], [3, 5], [4, 1]])
-    fig = plt.figure()
-    camera = Camera(fig)  # creating camera for animation
-
-    if polar:
-        pts = from_polar(P)
-        bezik = bezi(pts, TAU)  # create curve and return values in all points of TAU
-        X, Y = bezik[:, 0], bezik[:, 1]  # as result get X and Y in 1 array
-        plt.axes(projection='polar')
-        phi = np.arctan(Y / X)
-        r = np.sqrt(np.square(X) + np.square(Y))
-
-        for i in range(100):
-            plt.plot(phi, r, color='#ff0000')
-            plt.scatter(phi[i * 10, 0], r[i * 10, 0], color='#0000ff', marker='s', s=100)
-            camera.snap()
-    else:
-        bezik = bezi(P, TAU)  # create curve and return values in all points of TAU
-        X, Y = bezik[:, 0], bezik[:, 1]  # as result get X and Y in 1 array
-        for i in range(100):  # 100 cadres of animation, marker = which line used for anim
-            plt.plot(X, Y, color='#ff0000')
-            plt.scatter(P[:, 0], P[:, 1], color='#000000', marker='o')
-            plt.scatter(X[i * 10, 0], Y[i * 10, 0], color='#0000ff', marker='s', s=100)
-            camera.snap()  # +cadre
-
-    animation = camera.animate(interval=20, repeat=False, repeat_delay=0)  # getting res of animation
-    animation.save('animation.gif', writer='pillow', fps=24)  # save res as .gif
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import sys
 
-import matplotlib
-import numpy as np
-from PyQt6 import QtWidgets  # PyQt 6.1.2 used to GUI
-from PyQt6.QtGui import QIcon, QMovie, QPixmap
-from matplotlib import pyplot as plt  # adding start plot
-from qt_material import apply_stylesheet  # set not system style
+import matplotlib  # to using Qt5Agg
+import numpy as np  # for evaluates
+from PyQt5 import QtWidgets  # PyQt 5 used to GUI
+from PyQt5.QtGui import QIcon  # for changing icon
+from qt_material import apply_stylesheet  # to set not system style https://pypi.org/project/qt-material/#usage
 
-from graph import new_plot  # main task of app
-from untitled import Ui_MainWindow
+from design import Ui_MainWindow  # import design file
 
-matplotlib.rcParams['figure.dpi'] = 100
+matplotlib.use('Qt5Agg')  # says that we will be using matplotlib's qt widget for graphs
 
 
 # Main window of my app
@@ -20,83 +18,58 @@ class MyWidget(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)  # for beauticode UI part forwarded to another file
         # so this line creates all items from revealed UI file
 
-        self.setWindowIcon(QIcon('icon.png'))  # changing app icon
-
-        self.Anim.clicked.connect(self.anim)  # connecting buttons with its functions
+        # changing app icon
+        self.setWindowIcon(QIcon('icon.png'))
+        # connecting button with its functions
+        self.Anim.clicked.connect(self.anim)
+        # connecting button with its functions
         self.Build.clicked.connect(self.build)
+        # connecting button with its functions
         self.polar.clicked.connect(self.to_polar_page)
-
+        # connecting button with its functions
         self.Anim_2.clicked.connect(self.anim_2)
+        # connecting button with its functions
         self.Build_2.clicked.connect(self.build_2)
+        # connecting button with its functions
         self.descartes.clicked.connect(self.to_descartes_page)
 
-        plt.plot()  # creating start plot
-        plt.savefig('yo.png')  # saving it
-        self.graph.setPixmap(QPixmap('yo.png'))  # and we can see it in the app
+    # start animation of graph in the descartes on clicking on button
+    def anim(self):
+        self.graph.anim()
 
-        plt.axes(projection='polar')
-        plt.plot()  # creating start plot
-        plt.savefig('yo.png')  # saving it
-        self.graph_2.setPixmap(QPixmap('yo.png'))  # and we can see it in the app
-
-    def build(self):  # builds our graph with animation
-
-        if all((self.x0.text(), self.y0.text(), self.x1.text(), self.y1.text(),
-                self.x2.text(), self.y2.text(), self.x3.text(), self.y3.text())):
-            P = np.array([[self.x0.text(), self.y0.text()],
-                          [self.x1.text(), self.y1.text()],
-                          [self.x2.text(), self.y2.text()],
-                          [self.x3.text(), self.y3.text()]], dtype=np.dtype(float))
-            new_plot(P)  # creating graph in .gif with new data
-            movie = QMovie('animation.gif')  # every click recreates new gif
-            # with new graph and ready for playing animation
-
-            movie.frameChanged.connect(self.stop)  # bcs of this gif doesn't loop
-            self.graph.setMovie(movie)  # add our gif to the label
-            self.graph.movie().start()  # starts and stops gif: we need only to see 1st cadre
-            self.graph.movie().stop()
-
-    def build_2(self):
-        if all((self.r0.text(), self.a0.text(), self.r1.text(), self.a1.text(),
-                self.r2.text(), self.a2.text(), self.r3.text(), self.a3.text())):
-            P = np.array([[self.a0.text(), self.r0.text()],
-                          [self.a1.text(), self.r1.text()],
-                          [self.a2.text(), self.r2.text()],
-                          [self.a3.text(), self.r3.text()]], dtype=np.dtype(float))
-            new_plot(P, polar=True)  # creating graph in .gif with new data
-            movie = QMovie('animation.gif')  # every click recreates new gif
-            # with new graph and ready for playing animation
-
-            movie.frameChanged.connect(self.stop)  # bcs of this gif doesn't loop
-            self.graph_2.setMovie(movie)  # add our gif to the label
-            self.graph_2.movie().start()  # starts and stops gif: we need only to see 1st cadre
-            self.graph_2.movie().stop()
-
-    def anim(self):  # start playing animation
-        self.graph.movie().start()
-
+    # start animation of graph in the polars on clicking on button
     def anim_2(self):
-        self.graph_2.movie().start()
+        self.graph_2.anim()
 
-    def stop(self):  # this stops our gif at the first/zero loop
-        movie = self.graph.movie()
-        movie_2 = self.graph_2.movie()
-        if movie.currentFrameNumber() == movie.frameCount() - 1:
-            movie.stop()
-        if movie_2.currentFrameNumber() == movie_2.frameCount() - 1:
-            movie_2.stop()
+    # build animation of graph in the descartes on clicking on button
+    def build(self):
+        P = np.array([[self.x0.text(), self.y0.text()],  #
+                      [self.x1.text(), self.y1.text()],
+                      [self.x2.text(), self.y2.text()],
+                      [self.x3.text(), self.y3.text()]],
+                     dtype=np.dtype(float))
+        self.graph.build(P)
 
-    def to_polar_page(self):
+    # build animation of graph in the polars on clicking on button
+    def build_2(self):
+        P = np.array([[self.r0.text(), self.a0.text()],
+                      [self.r1.text(), self.a1.text()],
+                      [self.r2.text(), self.a2.text()],
+                      [self.r3.text(), self.a3.text()]],
+                     dtype=np.dtype(float))
+        self.graph_2.build(P)
+
+    def to_polar_page(self):  # open polar form
         self.stackedWidget.setCurrentIndex(1)
 
-    def to_descartes_page(self):
+    def to_descartes_page(self):  # open descartes form
         self.stackedWidget.setCurrentIndex(0)
 
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    apply_stylesheet(app, theme='dark_teal.xml')
+if __name__ == "__main__":  # starts app
+    app = QtWidgets.QApplication([])  # create application
+    apply_stylesheet(app, theme='dark_teal.xml')  # set custom theme
     widget = MyWidget()
-    widget.show()
+    widget.show()  # show our main window
 
-    sys.exit(app.exec())
+    sys.exit(app.exec())  # closing of app
